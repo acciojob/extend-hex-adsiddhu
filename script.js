d89bf02aa153f55dc86a112a374042d1a729dd88
Adarsh Singh Siddhu <111896979+adsiddhu@users.noreply.github.com>
2023-12-09 09:57:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
 	length = shortHex.length
-	if (length == 4) {
-		console.log(`# ${shortHex[1] ${shortHex[1] ${shortHex[2] ${shortHex[2] ${shortHex[3] ${shortHex[3]}`)
-	}
-	else{
-		console.log(`# ${shortHex[0] ${shortHex[0] ${shortHex[1] ${shortHex[1] ${shortHex[2] ${shortHex[2]}`)
-	}
+    if (length == 4) {
+        console.log(`#${shortHex[1]}${shortHex[1]}${shortHex[2]}${shortHex[2]}${shortHex[3]}${shortHex[3]}`)
+    }
+    else {
+        console.log(`#${shortHex[0]}${shortHex[0]}${shortHex[1]}${shortHex[1]}${shortHex[2]}${shortHex[2]}`)
+    }
 };
 
 // Do not change the code below.
